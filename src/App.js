e1c6b2f6b4547ee70fa38325144c0cf185d6578d
thalyta <thalyta.conrado@gmail.com>
2021-07-31 19:02:06 -0400

    corrigi problema na listagem. closes #1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import { useState } from "react";
-import Formulario from "./compentes/Formulario";
-import Titulo from "./compentes/Titulo";
+import Form from "./componentes/Form";
+import Listing from "./componentes/Listing"; 
 
 const App = () => {
 
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
+  const [users, setUsers] = useState([{
+    name: "teste",
+    email: "teste@gmail",
+    cell: "0800"
+  }])
 
-  const recebeNome = (name) => {
-    setFirstName(name)
-  }
-
-  const recebeSobrenome = (lastName) => {
-    setLastName(lastName)
+  const addUser = (user) => {
+    setUsers(valorAntigo => {
+      return[
+      ...valorAntigo,
+      user]
+    })
   }
 
   return (
     <>
-      <Titulo firstName = {firstName} lastName = {lastName} />
-      <Formulario recebeNome = {recebeNome} recebeSobrenome = {recebeSobrenome}/>
+      <Form addUser = {addUser} />
+      <Listing users = {users} />
     </>
   );
 }
